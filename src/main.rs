972395f8104d24249e2b33d4db8ecf5662da7a91
Joan Rieu <joan@joanrieu.com>
2019-07-22 02:57:11 +0200

    use nalgebra lib

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,25 @@
+extern crate nalgebra;
+
 type Scene = Vec<Mesh>;
 
 struct Mesh {
-    transform: Transform,
+    position: Vec3,
     geometry: Geometry,
     material: Material,
 }
 
-struct Transform {
-    position: Vec3,
+impl Mesh {
+    fn distance(self, point: &Vec3) -> f32 {
+        match self.geometry {
+            Geometry::Sphere { radius } => (point - self.position).norm() - radius,
+        }
+    }
 }
 
-type Vec3 = (f32, f32, f32);
+type Vec3 = nalgebra::Vector3<f32>;
 
 enum Geometry {
     Sphere { radius: f32 },
-}
-
-impl Geometry {
-    fn distance(self, point: &Vec3) -> f32 {
-        match self {
-            Geometry::Sphere { radius } => radius,
-        }
-    }
 }
 
 struct Material {
@@ -31,12 +29,12 @@ struct Material {
 type Color = (u8, u8, u8);
 
 struct Camera {
-    viewPlaneNormal: Vec3,
-    viewUpVector: Vec3,
-    viewingReferencePoint: Vec3,
-    projectionReferencePoint: Vec3,
-    fieldOfView: f32,
-    aspectRatio: f32,
+    eye: Vec3,
+    target: Vec3,
+    aspect: f32,
+    fovy: f32,
+    znear: f32,
+    zfar: f32
 }
 
 struct RendererSettings {
@@ -49,20 +47,18 @@ fn render(scene: &Scene, camera: &Camera, settings: &RendererSettings) {
 
 fn main() {
     let scene = vec![Mesh {
-        transform: Transform {
-            position: (3.0, 2.0, -10.0),
-        },
+        position: Vec3::new(3.0, 2.0, -10.0),
         geometry: Geometry::Sphere { radius: 1.0 },
         material: Material { color: (1, 2, 3) },
     }];
     let camera = Camera {
-        viewPlaneNormal: (0.0, 0.0, -1.0),
-        viewUpVector: (0.0, 1.0, 0.0),
-        viewingReferencePoint: (0.0, 0.0, 10.0),
-        projectionReferencePoint: (0.0, 0.0, 0.0),
-        fieldOfView: 90.0,
-        aspectRatio: 1.5,
-    };
+    eye: Vec3::new(0.0, 0.0, -10.0),
+    target: Vec3::new(0.0, 0.0, 0.0),
+    aspect: 3.0 / 2.0,
+    fovy: 3.14 / 4.0,
+    znear: 1.0,
+    zfar: 1000.0
+} ;
     let settings = RendererSettings { definition: 600 };
     render(&scene, &camera, &settings)
 }
